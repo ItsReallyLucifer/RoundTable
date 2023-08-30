725036603f6f0b04adb7a84acba8009503915086
ItsReallyLucifer <itsreallylucifer@gmail.com>
2023-08-29 22:20:57 -0700

    Created, Connected database

---
+++ models/Room.ts
@@ -0,0 +1,9 @@
+// models/User.js
+
+import mongoose from 'mongoose'
+
+const RoomSchema = new mongoose.Schema({
+ status: String, 
+})
+
+export default mongoose.models.Room || mongoose.model('Room', RoomSchema)
